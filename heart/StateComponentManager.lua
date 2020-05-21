d4b9d94226a59e288adaeee52527ed12d018eeaa
Mikael Lind <elemel@spotify.com>
2020-05-21 15:04:21 +0200

    Fix state-component grouping

--- heart/StateComponentManager.lua
+++ heart/StateComponentManager.lua
@@ -30,7 +30,7 @@ function M:createComponent(id, config)
   self.states[id] = state
 
   if state then
-    heartTable.set2(self.states, state, id, true)
+    heartTable.set2(self.stateEntitySets, state, id, true)
   end
 end
 
@@ -38,7 +38,7 @@ function M:destroyComponent(id)
   local state = self.states[id]
 
   if state then
-    heartTable.set2(self.states, state, id, nil)
+    heartTable.set2(self.stateEntitySets, state, id, nil)
   end
 
   self.states[id] = nil
@@ -53,13 +53,13 @@ function M:setState(id, state)
     end
 
     if oldState then
-      heartTable.set2(self.states, oldState, id, nil)
+      heartTable.set2(self.stateEntitySets, oldState, id, nil)
     end
 
     self.states[id] = state
 
     if state then
-      heartTable.set2(self.states, state, id, true)
+      heartTable.set2(self.stateEntitySets, state, id, true)
     end
   end
 end
